0eea69a0327cdf488a9f7421b5eb34e662953987
nuo1234 <2637063100@qq.com>
2023-07-03 20:31:40 +0800

    second commit

---
+++ src/main/java/com/wc/git/GITTest.java
@@ -0,0 +1,13 @@
+package com.wc.git;
+
+/**
+ * @Author: Wc
+ * @Description:
+ * @Date: Created in 20:30 2023/7/3
+ * @Modified By:
+ */
+public class GITTest {
+    public static void main(String[] args) {
+        System.out.println("git-1");
+    }
+}
